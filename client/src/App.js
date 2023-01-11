42e7e31f07f963c5872d30befe34687b64d83614
14harish <harishpc2003@gmail.com>
2023-01-11 11:57:16 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,23 @@ import React,{useEffect,useState} from 'react';
 import './App.css';
 
 function App() {
- 
+  const  [Username,SetUsername]=useState('');
+  const  [path,SetPath]=useState('');
+  const  [age,SetAge]=useState('');
+
   return (
     <div className="App">
      <form>
       <label>Profile Photo:</label>
-      <input type="file"/><br/>
+      <input type="file" name='path' onChange={(e)=>{
+        SetPath(e.target.value)
+      }}/><br/>
       <label>Name:</label>
-      <input type="text"/><br/>
+      <input type="text" name='Username' onChange={(e)=>{
+        SetUsername(e.target.value)}}/><br/>
       <label>Age:</label>
-      <input type="text"/><br/>
+      <input type="text" name='age' onChange={(e)=>{
+        SetAge(e.target.value)}}/><br/>
       <input type="submit" value="submit"/>
      </form>
     </div>
--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
+const { json } = require('body-parser');
 const express=require('express');
+const fs=require('fs');
 
 const exp=express();
 
-exp.use("/api",(req,res)=>{
-    res.json({"users":["user1","user2"]});
+const data=fs.readFileSync("data.json");
+// const myValue=JSON.parse(data);
+
+
+// myValue.push(value);
+
+exp.get("/",(req,res)=>{
+    let value={
+        profilePath:path,
+        name:Username,
+        age:age,
+    }
+    const val=JSON.stringify(value);
+    fs.writeFileSync("data.json",val,(err)=>{
+        if (err) throw err;
+        console.log("data added");
+    })
+    res.send(value);
 })
+
+// exp.use("/api",(req,res)=>{
+//     res.json({"users":["user1","user2"]});
+// })
 
 exp.listen(8000,(req,res)=>{
     console.log("Connected with Server");
