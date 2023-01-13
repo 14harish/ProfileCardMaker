2888b28731992ca2ec54a5dda5e1576fcdbdfa8b
14harish <harishpc2003@gmail.com>
2023-01-13 08:33:39 +0530

    new

--- client/src/card.js
+++ client/src/card.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Data from './data.json';
 import './card.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
 
 
 function Card(){
@@ -12,7 +13,7 @@ function Card(){
                     <div className='card'>
                         <h1>Professional Card</h1>
                         <img src={data.profilePath} width="100px" height="100px"/>  
-                        <p><FontAwesomeIcon icon="fa-solid fa-user"/>Name:<span>{data.name}</span></p>
+                        <p>Name:<span>{data.name}</span></p>
                         <p>Profession:     <span>{data.profession}</span></p>
                         <p>Email-Id:       <span>{data.email}</span></p>
                         <p>Phone-No:       <span>{data.phoneNo}</span></p>
