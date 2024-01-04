e925fefc1233ca8d1dfb650f2ce76009c354be6c
14harish <harishpc2003@gmail.com>
2024-01-04 13:17:04 +0530

    new

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 // const express=require('express');
 // const fs=require('fs');
 // const body_parser=require('body-parser');
-// const cors=require('cors');
+const cors=require('cors');
 // const multer =require('multer');
 // const exp=express();
 
